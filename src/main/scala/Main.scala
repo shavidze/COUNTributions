8165947f28035c39030ffc300c6cb7026e878a7d
nghurtch <nghurtch@broadinstitute.com>
2022-06-05 15:59:59 +0400

    refactor Main.scala, provide layers to the main app

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,74 +1,54 @@
-import RepositoryRetriever.{RepositoryEnv, fetchRepositories}
-import ContributorRetriever.{ContributorsEnv, fetchContributors}
-import zio.*
-import zhttp.http.*
-import zhttp.service.{Client, Server}
-import zio.console.{Console, getStrLn, putStrLn}
 
-object Main extends App {
+import ContributorsRetriever.ContributorsEnv
+import Utils.{GithubTokenLoader, Token}
+import RepositoriesRetriever.RepositoriesEnv
+import io.circe.generic.auto._
+import io.circe.syntax.EncoderOps
+import sttp.client3.{HttpClientSyncBackend, Identity, SttpBackend}
+import zhttp.http._
+import zhttp.service.{ChannelFactory, EventLoopGroup, Server}
+import zio._
+import zio.console._
+import Utils._
 
-  type ServicesEnv = RepositoryEnv & ContributorsEnv
 
-  private def organizationUrl(organization: String, page: Int): String = s"https://api.github.com/orgs/$organization/repos?per_page=100&page=$page"
+object Main extends zio.App {
 
-  private def contributorsUrl(organization: String, repositoryName: String, page: Int): String = s"https://api.github.com/repos/$organization/$repositoryName/contributors?per_page=100&page=$page"
+  def organizationUrl(organization: String, page: Int = 1): String = s"https://api.github.com/orgs/$organization/repos?per_page=100&page=$page"
 
-  val env: ZLayer[Any, Nothing, ServicesEnv] = RepositoryRetriever.live ++ ContributorRetriever.live
+  def contributorsUrl(organization: String, repositoryName: String, page: Int): String = s"https://api.github.com/repos/$organization/$repositoryName/contributors?per_page=100&page=$page"
 
-  override def run(args: List[String]) = Server.start(8080, http).provideCustomLayer(env).exitCode
+  val backend: SttpBackend[Identity, Any] = HttpClientSyncBackend()
 
-  val http: Http[ServicesEnv with Console, Throwable, Request, Response] = Http.collectZIO[Request] {
-    case Method.GET -> _ / "org" / orgName / "contributors" => {
+  implicit val token: Token = GithubTokenLoader.loadToken
+
+  private val httpApp = Http.collectZIO[zhttp.http.Request] {
+    case Method.GET -> _ / "org" / organizationName / "contributors" =>
       for {
-        _    <- fetchRepositories("some organization", 1)
-        _    <- fetchContributors("some repo name", 1)
-        resp <- ZIO.succeed(Response.text("yes"))
-      } yield resp
-      
-    }
+        _            <- putStrLn(s"fetching repositories for $organizationName")
+        repositories <- RepositoriesRetriever.fetchRepositories(organizationName)
+        _            <- putStrLn(s"got repositories") *> putStrLn("fetching contributors in parallel")
+        contributors <- ZIO.foreachPar(repositories)(repository => ContributorsRetriever.fetchContributors(organizationName, repository)).map(_.reduce(MapCombiner.combine))
+        _            <- putStrLn("got contributors") *> putStrLn("processing contributors")
+        result       <- ZIO.succeed(contributors.map(_.toResult))
+        sortedResult <- ZIO.succeed(result.toSeq.sortWith(_.contributions > _.contributions))
+        resultAsJson <- ZIO.effect(sortedResult.asJson)
+        _            <- putStrLn("done") *> putStrLn(s"result = $resultAsJson")
+        response     <- ZIO.succeed(zhttp.http.Response.text(s"$resultAsJson"))
+      } yield response
   }
 
+  val httpServerLayers: ZLayer[Any, Nothing, ChannelFactory with EventLoopGroup] = ChannelFactory.auto ++ EventLoopGroup.auto(8)
+  val repositoryLayer: ZLayer[Any, Nothing, RepositoriesEnv] = RepositoriesRetriever.live
+  val contributorsLayer: ZLayer[Any, Nothing, ContributorsEnv] = ContributorsRetriever.live
+  val httpAppLayer = httpServerLayers ++ repositoryLayer ++ contributorsLayer ++ Console.live
+
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] =
+    Server.start(8080, httpApp).provideLayer(httpAppLayer).exitCode
 }
 
-object RepositoryRetriever {
 
-  type RepositoryEnv = Has[RepositoryRetriever.Service]
 
-  trait Service {
-    def fetchRepositories(organizationName: String, pageCount: Int): Task[Unit]
-  }
 
-  val live: ZLayer[Any, Nothing, RepositoryEnv] = ZLayer.succeed {
-    new Service {
-      override def fetchRepositories(organizationName: String, pageCount: Int): Task[Unit] = Task.succeed {
-        println("fetching repositories...")
-      }
-    }
-  }
 
-  def fetchRepositories(organizationName: String, pageCount: Int): ZIO[RepositoryEnv, Throwable, Unit] =
-    ZIO.accessM(_.get.fetchRepositories(organizationName, pageCount))
-
-}
-
-object ContributorRetriever {
-
-  type ContributorsEnv = Has[ContributorRetriever.Service]
-
-  trait Service {
-    def fetchContributors(repositoryName: String, pageCount: Int): Task[Unit]
-  }
-
-  val live: ZLayer[Any, Nothing, ContributorsEnv] = ZLayer.succeed {
-    new Service {
-      override def fetchContributors(repositoryName: String, pageCount: Int): Task[Unit] = Task.succeed {
-        println("fetching contributors...")
-      }
-    }
-  }
-
-  def fetchContributors(repositoryName: String, pageCount: Int): ZIO[ContributorsEnv, Throwable, Unit] =
-    ZIO.accessM(_.get.fetchContributors(repositoryName, pageCount))
-
-}
 
